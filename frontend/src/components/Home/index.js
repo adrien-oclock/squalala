61ef2f24b44041327064135885b6a8cf380a19aa
Adrien O'clock <adrien@oclock.io>
2022-04-24 15:54:36 +0200

    WIP slider

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,21 +1,104 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
+
+import Slider from 'react-slick';
 
 import './styles.scss';
 
-const Home = () => (
-  <div>
-    <Carousel>
-      <div>Tototo</div>
-      <div>Tititi</div>
-      <div>Tatata</div>
-    </Carousel>
-    <Carousel>
-      <div>Tototo</div>
-      <div>Tititi</div>
-      <div>Tatata</div>
-    </Carousel>
-  </div>
-);
+const Home = function () {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <section id="home">
+      <div className="carouselContainer">
+        <h2>5 derniers soundboards</h2>
+        <Slider {...settings}>
+          <div className="carouselElement">
+            <p className="title">Tototo</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+          <div className="carouselElement">
+            <p className="title">Tatata</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+          <div className="carouselElement">
+            <p className="title">Tititi</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+      <div className="carouselContainer">
+        <h2>Top 5</h2>
+        <Slider>
+          <div className="carouselElement">
+            <p className="title">Tototo</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+          <div className="carouselElement">
+            <p className="title">Tatata</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+          <div className="carouselElement">
+            <p className="title">Tititi</p>
+            <p className="author">Adrien</p>
+            <p className="theme">Toto - Tata - Titi</p>
+            <div className="rating">
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star checked" />
+              <span className="fa fa-star" />
+              <span className="fa fa-star" />
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
