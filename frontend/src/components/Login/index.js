ca6aa4f48099d76f881d73bf2e4a184a0ea9bd1c
Adrien O'clock <adrien@oclock.io>
2022-04-26 11:37:47 +0200

    Add working popup

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { useEffect, useState } from 'react';
 import Popup from '../Popup';
 import './styles.scss';
 
@@ -10,54 +11,48 @@ const Login = () => {
   };
 
   return (
-  <div>
-    <button onClick={(e) => setVisibility(!visibility)} type="button">Connexion</button>
-    <Popup onClose={popupCloseHandler} show={visibility}>
-      <div className="modal">
-        <button className="close" onClick={close}>
-          &times;
-        </button>
-        <div className="modalContainer">
-          <section id="register-form">
-            <h3>Inscription</h3>
-            <form className="formContainer">
-              <div className="form-input-material">
-                <input type="text" name="username" id="register-username" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="register-username">Pseudo</label>
-              </div>
-              <div className="form-input-material">
-                <input type="email" name="email" id="register-email" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="register-email">Email</label>
-              </div>
-              <div className="form-input-material">
-                <input type="password" name="password" id="register-password" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="register-password">Mot de passe</label>
-              </div>
-              <div className="form-input-material">
-                <input type="password" name="password" id="register-password-confirm" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="register-password-confirm">Renseigner le même mot de passe</label>
-              </div>
-              <button type="submit" className="btn btn-primary btn-ghost">S'inscrire</button>
-            </form>
-          </section>
-          <section id="login-form">
-            <h3>Connexion</h3>
-            <form className="formContainer">
-              <div className="form-input-material">
-                <input type="email" name="email" id="login-email" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="login-email">Email</label>
-              </div>
-              <div className="form-input-material">
-                <input type="password" name="password" id="login-password" placeholder=" " autoComplete="off" className="form-control-material" required />
-                <label htmlFor="login-password">Mot de passe</label>
-              </div>
-              <button type="submit" className="btn btn-primary btn-ghost">Se connecter</button>
-            </form>
-          </section>
-        </div>
-      </div>
-    </Popup>
-  </div>
-)};
+    <div>
+      <button onClick={() => setVisibility(!visibility)} type="button">Connexion</button>
+      <Popup onClose={popupCloseHandler} show={visibility}>
+        <section id="register-form">
+          <h3>Inscription</h3>
+          <form className="formContainer">
+            <div className="form-input-material">
+              <input type="text" name="username" id="register-username" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="register-username">Pseudo</label>
+            </div>
+            <div className="form-input-material">
+              <input type="email" name="email" id="register-email" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="register-email">Email</label>
+            </div>
+            <div className="form-input-material">
+              <input type="password" name="password" id="register-password" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="register-password">Mot de passe</label>
+            </div>
+            <div className="form-input-material">
+              <input type="password" name="password" id="register-password-confirm" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="register-password-confirm">Renseigner le même mot de passe</label>
+            </div>
+            <button type="submit" className="btn btn-primary btn-ghost">S'inscrire</button>
+          </form>
+        </section>
+        <section id="login-form">
+          <h3>Connexion</h3>
+          <form className="formContainer">
+            <div className="form-input-material">
+              <input type="email" name="email" id="login-email" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="login-email">Email</label>
+            </div>
+            <div className="form-input-material">
+              <input type="password" name="password" id="login-password" placeholder=" " autoComplete="off" className="form-control-material" required />
+              <label htmlFor="login-password">Mot de passe</label>
+            </div>
+            <button type="submit" className="btn btn-primary btn-ghost">Se connecter</button>
+          </form>
+        </section>
+      </Popup>
+    </div>
+  );
+};
 
 export default Login;
