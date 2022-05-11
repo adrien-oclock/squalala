53e445ac413b771cca418fb98e660f77a8b58a62
Adrien O'clock <adrien@oclock.io>
2022-05-11 16:41:24 +0200

    Add search interaction for user list

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,32 @@
+export const formatData = (data) => {
+  const result = [];
+
+  for (const elements of data) {
+    const entity = elements[0];
+    entity.rating = elements['rating'] ? parseFloat(elements['rating']) : 1;
+
+    result.push(entity);
+  }
+
+  return result;
+}
+
+export const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const months = [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ]
+
+  return date.getDay() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
+}
